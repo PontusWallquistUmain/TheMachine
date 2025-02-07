e9a1ebfc6ae9672d6112f2049cc94c7109d79b0a
felix-umain <felix.wallquist@umain.com>
2025-02-07 19:49:47 +0100

    generate lrc

--- modules/transciber.py
+++ modules/transciber.py
@@ -3,21 +3,28 @@ import time
 
 model_name = "medium.en"
 model = whisper.load_model(model_name)
-
-def transcribe_lyrics(file_path):
-    return model.transcribe(file_path, fp16=False)
     
 def format_timestamp(seconds):
     minutes = int(seconds // 60)
     seconds = seconds % 60
     return f"{minutes:02}:{seconds:05.2f}"
 
+def write_to_lrc(transcription, file_path):
+    output_path = file_path[:file_path.rfind('/')] + ".lrc"
+
+    with open(output_path, "w", encoding="utf-8") as lrc_file:
+        for segment in transcription['segments']:
+            start_time = format_timestamp(segment['start'])
+            text = segment['text'].strip()
+            lrc_file.write(f"[{start_time}]{text}\n")
+
+def transcribe_lyrics(file_path):
+    result = model.transcribe(file_path, fp16=False)
+    write_to_lrc(result, file_path)
+    return result
+
 if __name__ == "__main__":
     start = time.time()
     result = transcribe_lyrics("audio/output/htdemucs/audio/vocals.mp3")
     end = time.time()
     print(f"Model: {model_name}\nTime: {end-start}")
-    for segment in result['segments']:
-        start = format_timestamp(segment["start"])
-        text = segment["text"]
-        print(f"start_time: [{start}] {text}")
