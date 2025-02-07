af9df49eadda3ac7c7a1dede0da51941df9dfc60
felix-umain <felix.wallquist@umain.com>
2025-02-07 19:10:33 +0100

    Update transcriber

--- modules/transciber.py
+++ modules/transciber.py
@@ -1,11 +1,23 @@
 import whisper
+import time
 
-model = whisper.load_model("turbo")
+model_name = "medium.en"
+model = whisper.load_model(model_name)
 
 def transcribe_lyrics(file_path):
     return model.transcribe(file_path, fp16=False)
+    
+def format_timestamp(seconds):
+    minutes = int(seconds // 60)
+    seconds = seconds % 60
+    return f"{minutes:02}:{seconds:05.2f}"
 
 if __name__ == "__main__":
-    #result = transcribe_lyrics("audio/input/audio.mp3")
+    start = time.time()
     result = transcribe_lyrics("audio/output/htdemucs/audio/vocals.mp3")
-    print(result["text"])
+    end = time.time()
+    print(f"Model: {model_name}\nTime: {end-start}")
+    for segment in result['segments']:
+        start = format_timestamp(segment["start"])
+        text = segment["text"]
+        print(f"start_time: [{start}] {text}")
